b0429a8cbc1f9dd40a568fabf78d62251de7e595
Vasiloudis <vasiloudis5@gmail.com>
2022-02-13 17:41:17 +0200

    Menu bar added

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import UserItem from "./UserItem";
 import './UsersList.css';
+import Card from "../../shared/components/UIElements/Card";
 
 const UserList = props => {
     if (props.items.length === 0){
-        return <div className="center">
-            <h2>No users found.</h2>
-        </div>
+        return (
+            <div className="center">
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
+            </div>
+        );
     }
     return <ul className="users-list">
         {props.items.map(user => (
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,8 +7,14 @@ function Users() {
     {
       id: 'u1', 
       name: 'Itachi Uchiha',
-      image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/84dc13b7-a2e7-4b45-83ec-311e72e82900/defzgw3-0afd0115-6bdf-41a0-9cbb-039e8542fbd4.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg0ZGMxM2I3LWEyZTctNGI0NS04M2VjLTMxMWU3MmU4MjkwMFwvZGVmemd3My0wYWZkMDExNS02YmRmLTQxYTAtOWNiYi0wMzllODU0MmZiZDQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.L2_a3xy7AQiAnrlKbxU5XRv75vwX5ipuFs159pKvODc', 
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbnY9FyJ94g6OibYVSVAEaRE_c69JHtK5Mgg&usqp=CAU', 
       places: 3
+    },
+    {
+      id: 'u2', 
+      name: 'Sasuke Uchiha',
+      image: 'https://cdn.myanimelist.net/r/360x360/images/characters/9/131317.jpg?s=b53e816a48dfacc4bc7768066596800c', 
+      places: 1
     }
   ];
 
