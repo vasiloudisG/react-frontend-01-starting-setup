d8462428918a04f1ddcb64fb9c778251e23873ee
Vasiloudis <vasiloudis5@gmail.com>
2022-02-26 21:07:52 +0200

    map bug (google maps deleted)

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,28 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 import './Map.css';
 
-const Map = props => {
-  const mapRef = useRef();
-  
-  const { center, zoom } = props;
-
-  useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom
-    });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
-
-  return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-    ></div>
-  );
-};
-
+const Map = () => {
+  return <h2>Map not added yet!</h2>
+}
 export default Map;
